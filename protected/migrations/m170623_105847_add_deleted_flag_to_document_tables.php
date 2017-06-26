02715ff7dbaa14cdceba49bd90f56311aa346263
Sabi <szabolcs.potondi@gmail.com>
2017-06-26 10:49:53 +0100

    OE-6706 : Deleted correspondence event stays in the Document output table with PENDING status - missing migration file

---
+++ protected/migrations/m170623_105847_add_deleted_flag_to_document_tables.php
@@ -0,0 +1,77 @@
+<?php
+
+class m170623_105847_add_deleted_flag_to_document_tables extends CDbMigration
+{
+	public function up()
+	{
+        $this->addColumn('document_set', 'deleted', "tinyint(1) unsigned NOT NULL DEFAULT '0'");
+        $this->addColumn('document_instance', 'deleted', "tinyint(1) unsigned NOT NULL DEFAULT '0'");
+        $this->addColumn('document_target', 'deleted', "tinyint(1) unsigned NOT NULL DEFAULT '0'");
+        $this->addColumn('document_instance_data', 'deleted', "tinyint(1) unsigned NOT NULL DEFAULT '0'");
+        $this->addColumn('document_output', 'deleted', "tinyint(1) unsigned NOT NULL DEFAULT '0'");
+
+        // loop over all the events to check if they are deleted
+        // get all correspondence where the status is not COMPLETED - completed document cannot be deleted
+        $document_outputs = DocumentOutput::model()->findAll();
+
+        foreach($document_outputs as $document_output){
+
+            $event = $document_output->getEvent();
+            if($event->deleted == 1) {
+
+                $transaction = Yii::app()->db->beginTransaction();
+                try {
+
+                    $document_target = $document_output->document_target;
+                    $document_target->deleted = 1;
+                    if (!$document_target->save()) {
+                        throw new Exception('Failed to save document_target: ' . print_r($document_target->getErrors(), true));
+                    }
+
+                    if( isset($document_target->document_instance_data) ){
+                        $document_target->document_instance_data->deleted = 1;
+                        if (!$document_target->document_instance_data->save()) {
+                            throw new Exception('Failed to save document_instance_data: ' . print_r($document_target->document_instance_data->getErrors(), true));
+                        }
+                    }
+
+                    if ($document_target->document_instance) {
+                        $document_target->document_instance->deleted = 1;
+                        if (!$document_target->document_instance->save()) {
+                            throw new Exception('Failed to save document_instance: ' . print_r($document_target->document_instance->getErrors(), true));
+                        }
+
+                        if ($document_target->document_instance->document_set) {
+                            $document_target->document_instance->document_set->deleted = 1;
+
+                            if (!$document_target->document_instance->document_set->save()) {
+                                throw new Exception('Failed to save document_set: ' . print_r($document_target->document_instance->document_set->getErrors(), true));
+                            }
+                        }
+                    }
+
+                    $document_output->deleted = 1;
+                    if(!$document_output->save()){
+                        throw new Exception('Failed to save document_output: ' . print_r($document_target->getErrors(), true));
+                    }
+
+
+                    $transaction->commit();
+
+                } catch (Exception $exception) {
+                    $transaction->rollback();
+                }
+            }
+
+        }
+	}
+
+	public function down()
+	{
+        $this->dropColumn('document_set', 'deleted');
+        $this->dropColumn('document_instance', 'deleted');
+        $this->dropColumn('document_target', 'deleted');
+        $this->dropColumn('document_instance_data', 'deleted');
+        $this->dropColumn('document_output', 'deleted');
+	}
+}
