68ecb8fafe150832bde8210a4e4c13250bcfa013
tomayoola <tom.ayoola@abehr.com>
2017-09-08 23:03:57 +0100

    Replaced sidebar.

--- protected/views/patient/_patient_element_sidebar.php
+++ protected/views/patient/_patient_element_sidebar.php
@@ -1,31 +1,70 @@
 <aside class="column sidebar episodes-and-events">
-    <div class="oe-scroll-wrapper" style="height:2000px">
-        <div class="all-panels"></div>
-    </div>
+  <div class="oe-scroll-wrapper" style="height:2000px">
+    <div class="all-panels"></div>
+
+    <h1 class="nsb-contents-header">Contents</h1>
+    <ul class="nsb-sidebar">
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Presenting History
+        </div>
+      </li>
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Ophalmic
+        </div>
+      </li>
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Medications
+        </div>
+      </li>
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Risks
+        </div>
+      </li>
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Social
+        </div>
+      </li>
+      <li class="nsb-element">
+        <div class="nsb-block">
+          Allergies
+        </div>
+      </li>
+    </ul>
+
+
+  </div>
 
 </aside>
 
-
-<script type="text/javascript">
-    new OpenEyes.UI.Sidebar(
-      $('.sidebar .oe-scroll-wrapper')
-    );
-
-    $(document).ready(function() {
-        event_sidebar = new OpenEyes.UI.PatientSidebar($('aside.episodes-and-events'), {
-            patient_sidebar_json: '<?php echo $this->getElementTree() ?>',
-            tree_id: 'patient-sidebar-elements'
-            <?php if ($this->event->id) {?>,
-            event_id: <?= $this->event->id ?>
-            <?php } ?>
-        });
-    });
-
-</script>
-
 <style>
-    .oe-event-sidebar-edit a.error {
-        background-color: #bf4040;
-        color: #fff;
-    }
+.nsb-sidebar {
+  list-style: none;
+  margin:0;
+}
+.nsb-block {
+  background-color: #777b7e;
+  color: #fff;
+  padding-top: 32px;
+  padding-left: 20px;
+  font-size: 1.0rem;
+  line-height: 14px;
+  border-bottom: #fff 1px solid;
+  height: 80px;
+  font-family: "Helvetica", Helvetica, Arial, sans-serif;
+}
+.nsb-contents-header {
+}
+
+
+
+
+.oe-event-sidebar-edit a.error {
+  background-color: #bf4040;
+  color: #fff;
+}
 </style>
