90e33d4f45a643537a5fc855660eba06d6dbaa6d
tomayoola <tom.ayoola@abehr.com>
2017-09-09 02:18:17 +0100

    Changed names.

--- protected/views/patient/_patient_element_sidebar.php
+++ protected/views/patient/_patient_element_sidebar.php
@@ -2,36 +2,36 @@
   <div class="oe-scroll-wrapper" style="height:2000px">
     <div class="all-panels"></div>
     <div class="nsb-sidebar-container">
-      <h1 class="nsb-contents-header">Contents</h1>
-      <ul class="nsb-sidebar">
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Presenting History
+      <h1 class="nsb-sidebar-header">Contents</h1>
+      <ul class="nsb-sidebar-list">
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            HISTORY
           </div>
         </li>
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Ophalmic
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            FUNCTION
           </div>
         </li>
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Medications
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            REFRACTION
           </div>
         </li>
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Risks
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            ANT SEGMENT
           </div>
         </li>
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Social
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            FUNDUS
           </div>
         </li>
-        <li class="nsb-element">
-          <div class="nsb-block">
-            Allergies
+        <li class="nsb-sidebar-element">
+          <div class="nsb-sidebar-block">
+            MANAGEMENT
           </div>
         </li>
       </ul>
@@ -41,37 +41,65 @@
   </div>
 
 </aside>
+<div class="nsb-popup-history">
+  Presenting Complaint
+</div>
 
 <style>
-.nsb-sidebar {
+.nsb-sidebar-list {
   list-style: none;
   margin:0;
 }
-.nsb-block {
+.nsb-sidebar-block {
   background-color: #777b7e;
   color: #dcdcdc;
   padding-top: 32px;
   padding-left: 16px;
   font-size: 1.0rem;
-  line-height: 14px;
+  line-height: 1.0rem;
   border-top: #9a9a9a 1px solid;
   height: 80px;
   font-family: "Helvetica", Helvetica, Arial, sans-serif;
   background-repeat: no-repeat;
   background-position: 97% 50%;
 }
-.nsb-block:hover {
+.nsb-sidebar-block:hover {
   color: #fff;
 }
-.nsb-contents-header {
+.nsb-sidebar-header {
   color: #0e386f;
   font-size: 1.5rem;
   font-style: italic;
   margin: 0;
   font-weight: 300;
+  line-height: 40px;
 }
 .nsb-sidebar-container {
+  margin-top: 0;
+}
+.nsb-popup-history{
+  position: fixed;
+  margin-left: 180px;
   margin-top: 40px;
+  z-index: 1100;
+  background-color: green;
+  width: 1045px; /* use .nsb-popup-* syntax or something*/
+  height: 480px;
+
+
+  background-color: #fff;
+  font-size: .8rem;
+  box-shadow: 2px 2px 2px 2px #666666;
+  border-radius: 7px;
+  font-family: Helvetica, Arial, sans-serif;
+  border: 1px solid #aaaaaa;
+  color: #222222;
+  box-sizing: border-box;
+  font-weight: normal;
+  font-style: normal;
+  line-height: 1.5;
+  cursor: auto;
+  background: #fff;
 }
 
 
