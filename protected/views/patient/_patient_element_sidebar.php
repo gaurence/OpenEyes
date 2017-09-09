d2983cdd4a80ddbbd69c12e04140613cf7c865f7
tomayoola <tom.ayoola@abehr.com>
2017-09-09 12:32:51 +0100

    Added some css.

--- protected/views/patient/_patient_element_sidebar.php
+++ protected/views/patient/_patient_element_sidebar.php
@@ -42,10 +42,122 @@
 
 </aside>
 <div class="nsb-popup-history">
-  Presenting Complaint
+  <span class="nsb-popup-cross-icon"></span>
+  <ul class="nsb-popup-row">
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+  </ul>
+  <ul class="nsb-popup-row">
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+    <li class="nsb-popup-element">
+      <div class="nsb-popup-block" id="nsb-popup-history-img">
+        <span class="nsb-popup-add-icon"></span>
+        <span class="nsb-popup-block-title">Presenting Complaint</span>
+      </div>
+    </li>
+  </ul>
 </div>
 
 <style>
+.nsb-popup-cross-icon {
+  background-size: 40px 40px;
+  display: block;
+  height: 40px;
+  width: 40px;
+  margin-top: 38px;
+  margin-left: 60px;
+  position: fixed;
+  z-index: 1101;
+}
+.nsb-popup-add-icon {
+  background-size: 40px 40px;
+  display: block;
+  height: 40px;
+  width: 40px;
+  margin-top: 38px;
+  margin-left: 60px;
+  position: fixed;
+}
+.nsb-popup-element {
+  float: left;
+  margin-right: 20px;
+}
+.nsb-popup-block-title {
+  font-family: "Helvetica", Helvetica, Arial, sans-serif;
+  color: #fff;
+  font-size: 0.9rem;
+}
+.nsb-popup-row {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: inline-block;
+  margin-bottom: 40px;
+}
+.nsb-popup-block {
+  width: 165px;
+  height: 128px;
+  background-repeat: no-repeat;
+  background-size: 165px 128px;
+  text-align: center;
+  align-content: center;
+  border-radius: 5px;
+  display: inline-block;
+  padding-top: 2px;
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0,0,0,.12), 0 2px 4px rgba(0,0,0,.24);
+  cursor: pointer;
+}
+.nsb-popup-block:hover {
+  opacity: 0.8;
+}
+#nsb-popup-history-img {
+  background-image:
+    linear-gradient(
+      to top,
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0.4)
+    ),
+    url(https://corticalchauvinism.files.wordpress.com/2015/02/doctor-taking-medication-history-ashx.jpg);
+}
 .nsb-sidebar-list {
   list-style: none;
   margin:0;
@@ -62,6 +174,7 @@
   font-family: "Helvetica", Helvetica, Arial, sans-serif;
   background-repeat: no-repeat;
   background-position: 97% 50%;
+  text-align: center;
 }
 .nsb-sidebar-block:hover {
   color: #fff;
@@ -85,21 +198,18 @@
   background-color: green;
   width: 1045px; /* use .nsb-popup-* syntax or something*/
   height: 480px;
-
-
+  font-family: "Helvetica", Helvetica, Arial, sans-serif;
   background-color: #fff;
   font-size: .8rem;
   box-shadow: 2px 2px 2px 2px #666666;
   border-radius: 7px;
   font-family: Helvetica, Arial, sans-serif;
   border: 1px solid #aaaaaa;
-  color: #222222;
+  /*color: #222222;*/
   box-sizing: border-box;
-  font-weight: normal;
-  font-style: normal;
-  line-height: 1.5;
-  cursor: auto;
-  background: #fff;
+  line-height: .8rem;
+  padding-left: 40px;
+  padding-top: 100px;
 }
 
 
